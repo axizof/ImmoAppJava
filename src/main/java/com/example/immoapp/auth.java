08d2c622e2674dc39b15e40e79fa4ac1b107c423
Axiz <38507086+axizof@users.noreply.github.com>
2023-09-27 14:14:46 +0200

    auth update login without db

--- src/main/java/com/example/immoapp/auth.java
+++ src/main/java/com/example/immoapp/auth.java
@@ -1,8 +1,15 @@
 package com.example.immoapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.*;
 
 public class auth {
@@ -12,10 +19,21 @@ public class auth {
 
     @FXML
     private TextField id;
-    // nv mess
     @FXML
     private Button connexion;
 
-    private String blabla;
+    @FXML
+    private void handleConnexion(ActionEvent event) throws IOException {
+        String enteredPassword = mdp.getText();
 
+        // Check if the entered password is equal to "admin"
+        if ("admin".equals(enteredPassword)) {
+            // Load the new scene (Main.fxml in this example)
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) connexion.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }
+    }
 }

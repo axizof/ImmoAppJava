af441592cafcb587085b4e01181fd2c7ff8a3e48
Axiz <38507086+axizof@users.noreply.github.com>
2023-09-27 10:51:48 +0200

    zefzef

--- src/main/java/com/example/immoapp/auth.java
+++ src/main/java/com/example/immoapp/auth.java
@@ -3,6 +3,7 @@ package com.example.immoapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import java.util.*;
 
 public class auth {
 
@@ -11,9 +12,10 @@ public class auth {
 
     @FXML
     private TextField id;
-
+    // nv mess
     @FXML
     private Button connexion;
 
-    //rfrfrf
+    private String blabla;
+
 }

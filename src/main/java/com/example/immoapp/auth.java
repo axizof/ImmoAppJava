979ded1418c04547f76df25bf9274c67573fe87d
Axiz <38507086+axizof@users.noreply.github.com>
2023-10-04 10:20:57 +0200

    update auth

--- src/main/java/com/example/immoapp/auth.java
+++ src/main/java/com/example/immoapp/auth.java
@@ -13,6 +13,9 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 public class auth {
@@ -28,15 +31,44 @@ public class auth {
     @FXML
     private void handleConnexion(ActionEvent event) throws IOException {
         String enteredPassword = mdp.getText();
+        String enteredLogin = login.getText();
 
-        // Check si pass = "admin"
+        // Check if the entered password is equal to "admin"
         if ("admin".equals(enteredPassword)) {
-            // load the new scene
+            // Load the new scene (Main.fxml in this example)
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) connexion.getScene().getWindow();
             Scene scene = new Scene(root);
             stage.setScene(scene);
+        }else {
+            Bdd bdd = new Bdd();
+            bdd.getConnexion();
+            try{
+                PreparedStatement login = bdd.con.prepareStatement("SELECT login,mdp FROM Employe WHERE login = ?");
+                login.setString(1, enteredLogin);
+                ResultSet resultat = login.executeQuery();
+                if(resultat.next()){
+                    if(resultat.getString(2).equals(enteredPassword)) {
+                        resultat.close();
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+                        Parent root = loader.load();
+                        Stage stage = (Stage) connexion.getScene().getWindow();
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                    }
+                }else{
+                    Alert a = new Alert(Alert.AlertType.ERROR);
+                    a.setContentText("Mauvais login/mot de passe");
+                    a.show();
+                }
+            }catch (SQLException e){
+                SQLLogException sqlLogException = new SQLLogException(e);
+                DatabaseManager.logError(sqlLogException);
+                System.out.println(e);
+            }
+
+
         }
     }
 }

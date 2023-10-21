32ec65476b171a08a3e836ca7ca4075b3acc858b
axizof <mrgamerdu56@gmail.com>
2023-10-21 20:32:11 +0200

    eeee

--- src/main/java/com/example/immoapp/AddEquipement.java
+++ src/main/java/com/example/immoapp/AddEquipement.java
@@ -2,6 +2,7 @@ package com.example.immoapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 import java.sql.*;
@@ -13,46 +14,53 @@ public class AddEquipement {
 
     @FXML TextField libelle;
 
-    public void addPiece(String libelle, String surface, int id_Logement) {
-        int CheckInfo;
-        try {
-            Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
-            String sql = "INSERT INTO equipement (libelle, surface, id_Logement) " +
-                    "VALUES (?, ?, ?)";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, libelle);
-            stmt.setString(2, surface);
-            stmt.setInt(3, id_Logement);
-            stmt.executeUpdate();
-            sql = "SELECT id FROM Piece WHERE libelle = ? AND surface = ? AND id_Logement = ?";
-            PreparedStatement stmt2 = conn.prepareStatement(sql);
-            stmt2.setString(1, libelle);
-            stmt2.setString(2, surface);
-            stmt2.setInt(3, id_Logement);
-            ResultSet rs = stmt2.executeQuery();
-            int idPieceInd = -1;
-            if (rs.next()) {
-                idPieceInd = rs.getInt("ID");
-            }
-            for (int i = 0; i < ImagesBase64.size(); i++) {
-                sql = "INSERT INTO Photo (id_Piece, photo) " +
-                        "VALUES (?, ?)";
-                PreparedStatement stmt3 = conn.prepareStatement(sql);
-                stmt3.setInt(1, idPieceInd);
-                stmt3.setString(2, ImagesBase64.get(i));
-                stmt3.executeUpdate();
-            }
-            stmt.close();
-            stmt2.close();
+    public void addEquipement(String libelle, int id_Piece) {
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
+            // Vérifiez d'abord si un équipement avec le même libellé existe déjà.
+            String checkSql = "SELECT id FROM Equipement WHERE libelle = ? AND id_Piece = ?";
+            PreparedStatement checkStmt = conn.prepareStatement(checkSql);
+            checkStmt.setString(1, libelle);
+            checkStmt.setInt(2, id_Piece);
+            ResultSet checkRs = checkStmt.executeQuery();
 
-            // Show a confirmation dialog
-            showConfirmationDialog();
+            if (checkRs.next()) {
+                // Un équipement avec le même libellé existe déjà, affichez une alerte.
+                showDuplicateEquipementAlert();
+            } else {
+                // L'équipement est unique, vous pouvez l'ajouter.
+                String insertSql = "INSERT INTO Equipement (libelle, id_Piece) VALUES (?, ?)";
+                PreparedStatement insertStmt = conn.prepareStatement(insertSql);
+                insertStmt.setString(1, libelle);
+                insertStmt.setInt(2, id_Piece);
+                insertStmt.executeUpdate();
 
-            // Close the window
-            closeWindow();
+                // Récupérez l'ID de l'équipement nouvellement ajouté, si nécessaire.
+
+                for (int i = 0; i < ImagesBase64.size(); i++) {
+                    String photoSql = "INSERT INTO Photo (id_Equipement, photo) VALUES (?, ?)";
+                    PreparedStatement photoStmt = conn.prepareStatement(photoSql);
+                    photoStmt.setInt(1, idEquipement); // Assurez-vous d'avoir l'ID de l'équipement
+                    photoStmt.setString(2, ImagesBase64.get(i));
+                    photoStmt.executeUpdate();
+                }
+
+                // Affichez une confirmation de l'ajout.
+                showConfirmationDialog();
+
+                // Fermez la fenêtre si nécessaire.
+                closeWindow();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void showDuplicateEquipementAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erreur");
+        alert.setHeaderText("Équipement en double");
+        alert.setContentText("Un équipement avec le même libellé existe déjà. Le nom doit être unique.");
+        alert.showAndWait();
     }
     public void BtnAjoutClick(ActionEvent actionEvent) {
 
